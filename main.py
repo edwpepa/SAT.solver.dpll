0a59fd77e6c06b35f23dc186934b0c66be745c70
edwpepa <eduard.pepa04@e-uvt.ro>
2025-05-11 20:22:45 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+# main.py
+# so this is the runner for my dpll sat solver
+# i made this to test on a few .cnf files i wrote by hand i dont exactly know if we 
+#should test on the ones you ,mr teacher, gave us
+# it shows time and result 
+
+import time
+from dpll_solver import dpll, parse_cnf
+
+# this runs the actual test on 1 file
+def run_experiment(file_path):
+    print("testing on:", file_path)
+    clauses, symbols = parse_cnf(file_path)
+
+    start = time.time()
+    result = dpll(clauses, symbols, [])
+    end = time.time()
+
+    if result:
+        print("SATIISFIABLE")
+    else:
+        print("UNSATISFIABLE")
+
+    print("took:", round((end - start)*1000, 3), "ms\n")
+
+# files i tested on 
+files = [
+    "../data/test1.cnf",
+    "../data/test2.cnf"
+]
+
+# loop through each one
+for f in files:
+    run_experiment(f)
